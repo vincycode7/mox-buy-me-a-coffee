69571833e865ad4380bde99babc312dade182a21
Gbenga Vincent Akinwande <gbengavincentakinwande@gmail.com>
2025-10-03 16:12:41 +0100

    deleted base project files

--- tests/conftest.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from script.deploy import deploy
-
-@pytest.fixture
-def counter_contract():
-    return deploy()
--- tests/test_counter.py
+++
@@ -1,3 +0,0 @@
-def test_increment(counter_contract):
-    counter_contract.increment()
-    assert counter_contract.number() == 2
